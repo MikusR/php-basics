4f4dad0ff07d0566ba79a42cb3a1f15acf71c20c
Mikus Rakis <mikus.rakis@gmail.com>
2023-09-26 20:13:06 +0300

    Atsevišķa izvade

--- tests/01.php
+++ tests/01.php
@@ -3,10 +3,14 @@
 $numOfEpisodesJson = json_decode(file_get_contents('https://rickandmortyapi.com/api/episode'));
 $numOfEpisodes = $numOfEpisodesJson->info->count;
 $episodes = [];
-for ($i = 1; $i <= $numOfEpisodes; $i++) {
+for ($i = 1; $i <= 3; $i++) {
 
     $tempFile = file_get_contents("https://rickandmortyapi.com/api/episode/$i");
     $tempJson = json_decode($tempFile);
     $episodes[$i] = "Episode " . $tempJson->episode . ' with title ' . "'$tempJson->name'" . ' aired ' . $tempJson->air_date;
-    echo $episodes[$i] . PHP_EOL;
+
+}
+
+foreach ($episodes as $episode) {
+    echo $episode . PHP_EOL;
 }
