cdf4dfa5101b25b4dae7efb1b6b4cf1f4f6d6533
Mikus Rakis <mikus.rakis@gmail.com>
2023-09-26 20:14:57 +0300

    fix

--- tests/01.php
+++ tests/01.php
@@ -3,7 +3,7 @@
 $numOfEpisodesJson = json_decode(file_get_contents('https://rickandmortyapi.com/api/episode'));
 $numOfEpisodes = $numOfEpisodesJson->info->count;
 $episodes = [];
-for ($i = 1; $i <= 3; $i++) {
+for ($i = 1; $i <= $numOfEpisodes; $i++) {
 
     $tempFile = file_get_contents("https://rickandmortyapi.com/api/episode/$i");
     $tempJson = json_decode($tempFile);
