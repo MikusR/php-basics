2881eeab93de22b9351eea0c85d7e4a8cd917d1b
Mikus Rakis <mikus.rakis@gmail.com>
2023-10-01 23:45:48 +0300

    06

---
+++ loops/06.php
@@ -0,0 +1,43 @@
+<?php
+//Write a console program in a class named AsciiFigure that draws a figure of the following form, using for loops.
+//
+// ////////////////\\\\\\\\\\\\\\\\
+// ////////////********\\\\\\\\\\\\
+// ////////****************\\\\\\\\
+// ////************************\\\\
+// ********************************
+//Then, modify your program using an integer class constant so that it can create a similar figure of any size.
+// For instance, the diagram above has a size of 5. For example, the figure below has a size of 3:
+//
+// ////////\\\\\\\\
+// ////********\\\\
+// ****************
+//And the figure below has a size of 7:
+//
+// ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\
+// ////////////////////********\\\\\\\\\\\\\\\\\\\\
+// ////////////////****************\\\\\\\\\\\\\\\\
+// ////////////************************\\\\\\\\\\\\
+// ////////********************************\\\\\\\\
+// ////****************************************\\\\
+// ************************************************
+
+
+function drawAsciiFigure(int $size): void
+{
+    for ($rinda = 0; $rinda < $size; $rinda++) {
+
+        for ($slash = $size - $rinda - 1; $slash > 0; $slash--) {
+            echo "////";
+        }
+        for ($star = 0; $star < $rinda; $star++) {
+            echo "********";
+        }
+        for ($backSlash = $size - $rinda - 1; $backSlash > 0; $backSlash--) {
+            echo '\\\\\\\\';
+        }
+        echo PHP_EOL;
+    }
+}
+
+drawAsciiFigure(2);
